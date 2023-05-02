91a8cea4ce91be5b3164318ae97c708fef4b5417
SAI GANESH <113955895+SaiGaneshoo1@users.noreply.github.com>
2023-05-02 14:36:02 +0530

    Update Affine Caesar cipher.cpp

--- Affine Caesar cipher.cpp
+++ Affine Caesar cipher.cpp
@@ -1,29 +1,85 @@
-l1 = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
-      'X', 'Y', 'Z']
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+#include<stdlib.h>
+int CalcGCD(int);
+main()
+{
+ int i,j,k,gcd,alpha,beta,numstr[100],numcipher[100];
+ char str[100],cipher[100];
+ printf("Enter a string\n");
+ gets(str);
 
-a = int(input("Enter the a value: "))
-b = int(input("Enter the b value: "))
-ch = int(input("Enter the choice:\n1)Encrypt\n2)Decrypt\t:"))
-if ch == 1:
-    pt = input("Enter the plain text: ")
-    alphabet = ""
-    for i in pt:
-        if i != " ":
-            if i in l1:
-                x = l1.index(i)
-                new_index = int(((a * x) + b) % 26)
-                alphabet = alphabet + l1[new_index]
+ for(i=0,j=0;i<strlen(str);i++)
+ {
+  if(str[i]!=' ')
+  {
+   str[j]=toupper(str[i]);   
+   j++;
+  }
+  else
+  {
+   str[j]=' ';
+   j++;
+  }
+ }
+ str[j]='\0';
+ printf("Entered string is : %s \n",str);
+ printf("Enter Alpha value and must be between 1 and 25 both included\n");
+ scanf("%d",&alpha); 
+ if(alpha<1 || alpha>25)
+ {
+  printf("Alpha should lie in between 1 and 25\nSorry Try again !\n");
+  exit(0);
+ } 
+ gcd=CalcGCD(alpha);
+ if(gcd!=1)
+ {
+  printf("gcd(alpha,26)=1 but \n gcd(%d,26)=%d\nSorry Try again !\n",alpha,gcd);
+  exit(0);
+ }
+ printf("Enter Beta value and must be between 0 and 25 both included\n");
+ scanf("%d",&beta);
+ if(beta<0 || beta>25)
+ {
+  printf("Beta value should lie between 0 and 25\nSorry Try again !\n");
+  exit(0);
+ }
+ for(i=0;i<strlen(str);i++)
+ {
+  if(str[i]!=' ')
+  numstr[i]=str[i]-'A';
+  else
+  numstr[i]=-20;
+ }
+    printf("Affine Cipher text is\n");   
+    for(i=0;i<strlen(str);i++)
+    {
+     if(numstr[i]!=-20)
+     {
+     numcipher[i]=((alpha*numstr[i])+beta)%26;
+          printf("%c",(numcipher[i]+'A'));         
+     }
+     else
+     {
+          printf(" ");      
+     }
+    }
+printf("\n");
+ 
+}
 
-    print(alphabet)
+int CalcGCD(int alpha)
+{
+ int x;
+   int temp1=alpha;
+   int temp2=26;
 
-if ch == 2:
-    ct = input("Enter the cipher text: ")
-    alphabet = ""
-    for i in ct:
-        if i != " ":
-            if i in l1:
-                x = l1.index(i)
-                new_index = int(((x - b) / a) % 26)
-                alphabet = alphabet + l1[new_index]
-                
-    print(alphabet)
+     while(temp2!=0)
+      {
+        x=temp2;
+        temp2=temp1%temp2;
+        temp1=x;
+      }
+     return(temp1);
+}
